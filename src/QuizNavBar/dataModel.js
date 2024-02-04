b27a581e15b23c07955500074c671d93afa8ce02
PatriciaBukovac <patricia.bukovac@deutschebahn.com>
2024-02-04 12:24:17 +0100

    initial commit

---
+++ src/QuizNavBar/dataModel.js
@@ -0,0 +1,29 @@
+export const questions = [
+  {
+    id: 123,
+    prompt: "What two things can you never eat for breakfast?",
+    answer: "Lunch and dinner"
+  },
+  {
+    id: 124,
+    prompt: "What word is spelled incorrectly in every single dictionary?",
+    answer: "Incorrectly"
+  },
+  {
+    id: 125,
+    prompt:
+        "What do you answer every day, even though it never asks you a question?",
+    answer: "Your phone"
+  },
+  {
+    id: 126,
+    prompt:
+        "What starts with “e” and ends with “e” but only has one letter in it?",
+    answer: "An envelope."
+  },
+  {
+    id: 127,
+    prompt: "What has a face and two hands, but no arms or legs?",
+    answer: "A clock"
+  }
+];
