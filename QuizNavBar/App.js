ec8780533939fbc43bf71f32e6f96afb922e8bb6
PatriciaBukovac <patricia.bukovac@deutschebahn.com>
2024-02-04 12:21:38 +0100

    initial commit

--- QuizNavBar/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import QuizNavBar from "./QuizNavBar";
-import {questions} from "./dataModel";
-
-export default function App() {
-  return <QuizNavBar questions={questions}/>;
-}
